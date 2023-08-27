c88f405b8380e238a9b533149d4a68ebd7f256c2
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-27 20:14:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(name='clean_folder',
-      version='0.0.1',
-      # packages=['cleen_folder'],
-      packages=find_packages(),
-      author='Daria_Zava',
-      author_email='dasha_kirik49@ukr.net',
-      description="clean folder from trash",
-      entry_points={
-          'console_scripts': ['clean-folder = clean_folder.clean:main',
-                              'fill-fils = clean_folder_files_generator:file_generator']
-
-      },
-      install_requires=[
-          'numpy',
-          'Pillow',
-      ],
-      )
+setup(
+    name='clean_folder',
+    version='0.1',
+    description='Clean folder program by Python',
+    author='DashaZava',
+    author_email='Dashss_kirik49@ukr.net',
+    url='https://github.com/DashaZava/Home_work7',
+    license='MIT',
+    packages=['clean_folder'],
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'clean-folder = clean_folder.clean:main']}
+)
