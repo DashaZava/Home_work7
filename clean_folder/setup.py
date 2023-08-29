cea25220de37652d1f960a813048252e25f5a425
DashaZava <112952295+DashaZava@users.noreply.github.com>
2023-08-29 09:43:56 +0100

    Create setup.py

---
+++ clean_folder/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='Clean folder program by Python',
+    author='DashaZava',
+    author_email='dasha)kirik49@ukr.net',
+    url='https://github.com/DashaZava/Home_work7',
+    license='MIT',
+    packages=['clean_folder'],
+    include_package_data=True,
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
+)
